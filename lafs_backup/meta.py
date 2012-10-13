fe103ae786836ebd5be4ff71366bfbec9c3e5fa2
Mike Kazantsev <mk.fraggod@gmail.com>
2012-10-14 04:39:30 +0600

    meta: simple fix for ply littering (and failing when not allowed to) issue

--- lafs_backup/meta.py
+++ lafs_backup/meta.py
@@ -9,6 +9,16 @@ import os, sys, re, errno, types
 from cffi import FFI
 
 
+# Try to work around insane "write_table" operations (which assume that
+#  they can just write lextab.py and yacctab.py in current dir), used by default.
+try: from ply.lex import Lexer
+except ImportError: pass
+else: Lexer.writetab = lambda s,*a,**k: None
+try: from ply.yacc import LRGeneratedTable
+except ImportError: pass
+else: LRGeneratedTable.write_table = lambda s,*a,**k: None
+
+
 
 class CStrACL(object):
 
